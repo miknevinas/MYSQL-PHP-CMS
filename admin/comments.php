059e4fe62676ceab7730e206e8fbcf126f861631
Adam <miknevinas@gmail.com>
2016-04-06 10:47:04 -0600

    started comments

--- admin/comments.php
+++ admin/comments.php
@@ -9,32 +9,32 @@
             <div class="container-fluid">
                 <!-- Page Heading -->
                 <div class="row">
-                    
-                        <h1 class="page-header">
-                            Welcome to Admin
-                            <small>Author</small>
-                        </h1>
+
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Author</small>
+                    </h1>
                     <?php 
                     if(isset($_GET['source'])) {
                         $source = $_GET['source'];
                     } else {
                         $source = '';
                     }
-                    
+
                     switch($source) {
                         case 'add_post';
-                        include 'includes/add_post.php';
-                        break;
-                            
+                            include 'includes/add_post.php';
+                            break;
+
                         case 'edit_post';
-                        include 'includes/edit_post.php';
-                        break;
-                            
+                            include 'includes/edit_post.php';
+                            break;
+
                         default:
-                        include 'includes/view_all_posts.php';
-                        break;
+                            include 'includes/view_all_comments.php';
+                            break;
                     }
-                    
+
                     ?>
                 </div>
             </div>
---
+++ admin/includes/view_all_comments.php
@@ -0,0 +1,87 @@
+<table class="table table-bordered table-hover">
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>Author</th>
+        <th>Comment</th>
+        <th>Email</th>
+        <th>Status</th>
+        <th>In Response to</th>
+        <th>Date</th>
+        <th>Approve</th>
+        <th>Reject</th>
+        <th>Delete</th>
+    </tr>
+</thead>
+
+<tbody>
+    <?php 
+    $query = "SELECT * FROM comments";
+    $select_comments = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($select_comments)) {
+        $comment_id = $row['comment_id'];
+        $comment_post_id = $row["comment_post_id"];
+        $comment_author = $row["comment_author"];
+        $comment_email = $row["comment_email"];
+        $comment_content = $row["comment_content"];
+        $comment_status = $row["comment_status"];
+        $comment_date = $row["comment_date"];
+     
+
+
+        echo "<tr>";
+        echo "<td>{$comment_id}</td>";
+        echo "<td>{$comment_author}</td>";
+        echo "<td>{$comment_content}</td>";
+
+
+//        $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
+//        $select_category_id = mysqli_query($connection, $query);
+//
+//        while($row = mysqli_fetch_assoc($select_category_id)) {
+//            $cat_id = $row["cat_id"];
+//            $cat_title = $row["cat_title"];
+//
+//            echo "<td>{$cat_title}</td>";
+//        }
+        echo "<td>{$comment_email}</td>";
+        echo "<td>{$comment_status}</td>";
+        
+        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+        $select_post_id_query = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_assoc($select_post_id_query)) {
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            
+            echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
+            
+        }
+        
+        
+        echo "<td>{$comment_date}</td>";
+  
+        echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
+        echo "<td><a href='posts.php?delete='>Reject</a></td>";
+        echo "<td><a href='posts.php?delete='>Delete</a></td>";
+        echo "</tr>";
+    }
+    ?>
+</tbody>
+</table>
+
+
+<?php
+////////Delete posts//////////
+if(isset($_GET['delete'])) {
+    $delete_post_id = $_GET['delete'];
+
+    $query = "DELETE FROM posts WHERE post_id = '{$delete_post_id}' ";
+    $delete_item = mysqli_query($connection, $query);
+    header("Location: posts.php");
+
+    if(!$delete_item) {
+        die("ERROR " . mysqli_error($connection));
+    }
+}
+?>
