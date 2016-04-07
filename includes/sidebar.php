224a632cc0bf296453535db889aa2b1765ebfdb5
Adam <miknevinas@gmail.com>
2016-04-07 08:27:55 -0600

    started building login functionality

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,14 +16,29 @@
             </span>
         </div>
         </form>
-        <!-- /.input-group -->
+    </div>
+    
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <input name="password" type="password" class="form-control" placeholder="Password">
+            </div>
+            <span clsss="form-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">Submit</button>
+            </span>
+        </form>
     </div>
 
     <!-- Blog Categories Well -->
     <div class="well">
        
         <?php 
-        $query = "SELECT * FROM categories LIMIT 4";
+        $query = "SELECT * FROM categories";
         $select_categories_sidebar = mysqli_query($connection, $query);
 
         ?>
@@ -41,8 +56,6 @@
                     ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-           
         </div>
         <!-- /.row -->
     </div>
