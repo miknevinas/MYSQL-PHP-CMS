40f9546a21835237f4a1eb8ed321101dd8c2dbb6
Adam <miknevinas@gmail.com>
2016-04-11 13:24:02 -0600

    upload

--- includes/header.php
+++ includes/header.php
@@ -10,18 +10,7 @@
         <meta name="author" content="">
 
         <title>Blog Home - Start Bootstrap Template</title>
-
-        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
         <link href="css/blog-home.css" rel="stylesheet">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-
 </head>
