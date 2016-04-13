e155a299e7eb099fdeb87f5e1466684248b003a9
Adam <miknevinas@gmail.com>
2016-04-13 10:17:10 -0600

    minor fixes

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-
-        <title>Blog Home - Start Bootstrap Template</title>
+        <title>Wordsmith/Codesmith</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/blog-home.css" rel="stylesheet">
